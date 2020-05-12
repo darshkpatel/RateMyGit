2bbf04552fabde50c58a6f16cc6194dbe22cd539
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-12 10:00:13 +0000

    Deploy darshkpatel/RateMyGit to github.com/darshkpatel/RateMyGit.git:gh-pages

---
+++ precache-manifest.f0ea659d87a2c71d66242c38c10df995.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffbaf5eaaed412369325a1c88004ef69",
+    "url": "/RateMyGit/index.html"
+  },
+  {
+    "revision": "59a94b5e642f49b7d45e",
+    "url": "/RateMyGit/static/css/2.ab7e5612.chunk.css"
+  },
+  {
+    "revision": "81d39851c76cc57d8fc4",
+    "url": "/RateMyGit/static/css/main.578da192.chunk.css"
+  },
+  {
+    "revision": "59a94b5e642f49b7d45e",
+    "url": "/RateMyGit/static/js/2.dd94c661.chunk.js"
+  },
+  {
+    "revision": "81d39851c76cc57d8fc4",
+    "url": "/RateMyGit/static/js/main.4e9b03eb.chunk.js"
+  },
+  {
+    "revision": "c626e084f4a6286c95b2",
+    "url": "/RateMyGit/static/js/runtime~main.8a49257e.js"
+  },
+  {
+    "revision": "f2ea7c2c06143d73281e6418bea662bc",
+    "url": "/RateMyGit/static/media/svg-banner.f2ea7c2c.svg"
+  }
+]);
